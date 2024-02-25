27290653075958a93d1115975032a294aff48f4b
Karl Rombauts <karlrombauts@gmail.com>
2024-02-25 13:50:20 +1100

    Add prepare build script

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,7 +1,10 @@
 export { EffectsBoardWrapper } from './components';
-export { EffectsContext, EffectsPropsContext, EffectsQueueContext } from "./contexts";
+export {
+  EffectsContext,
+  EffectsPropsContext,
+  EffectsQueueContext,
+} from './contexts';
 export { useEffectListener } from './hooks/useEffectListener';
 export { useEffectQueue } from './hooks/useEffectQueue';
 export { useEffectState } from './hooks/useEffectState';
 export { useLatestPropsOnEffect } from './hooks/useLatestPropsOnEffect';
-
